c13ea2dd6aa12b5ab75e79e2bd0ff2c3d37fc458
Taras-OK <taraspm35@gmail.com>
2016-04-28 00:35:53 +0300

    Created method "if file added" 28.04.2016

--- src/com/company/prog/m2/findFiles/Main.java
+++ src/com/company/prog/m2/findFiles/Main.java
@@ -7,38 +7,70 @@ import java.util.ArrayList;
 
 public class Main {
 
-	static class MyFileFilter implements FilenameFilter {
-		private String ext;
+    static class MyFileFilter implements FilenameFilter {
+        private String ext;
 
-		public MyFileFilter(String ext) {
-			this.ext = ext;
-		}
+        public MyFileFilter(String ext) {
+            this.ext = ext;
+        }
 
-		public boolean accept(File dir, String name) {
-			return name.endsWith(ext);
-		}
-	}
+        public boolean accept(File dir, String name) {
+            return name.endsWith(ext);
+        }
+    }
 
-	private static void findFiles(String srcPath, String ext, ArrayList<String> list) throws IOException {
-		File dir = new File(srcPath);
-		File[] files = dir.listFiles(new MyFileFilter(ext));
+    private static void findFiles(String srcPath, String ext, ArrayList<String> list) throws IOException {
+        File dir = new File(srcPath);
+        File[] files = dir.listFiles(new MyFileFilter(ext));
 
-		for (int i = 0; i < files.length; i++) {
-			list.add(srcPath + files[i].getName());
-		}
-	}
+        for (int i = 0; i < files.length; i++) {
+            list.add(srcPath + files[i].getName());
+        }
+    }
 
-	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
+    private static void findNewFile() {
 
-		try {
-			findFiles("d:\\", ".txt", list);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    }
 
-		for (String s : list)
-			System.out.println(s);
-	}
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list1 = new ArrayList<String>();
+
+        try {
+            findFiles("d:\\", ".txt", list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("d:\\" + " contain: ");
+
+        for (String s : list)
+            System.out.println(s);
+
+        System.out.println("_____________________");
+
+        int count = list.size();
+
+        while (true) {
+            try {
+                findFiles("d:\\", ".txt", list1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (list1.size() > count) {
+                System.out.println();
+                System.out.println("New file added: " + list1.get(0).toString());
+                count++;
+            } else {
+                System.out.print(".");
+            }
+            list1.clear();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
 
 }
