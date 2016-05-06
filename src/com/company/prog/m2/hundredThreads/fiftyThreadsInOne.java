b947c7f069cc034d4b946cd53c9e40284387e239
Taras-OK <taraspm35@gmail.com>
2016-05-07 00:08:47 +0300

    Added HundredThreads 6.05.2016

---
+++ src/com/company/prog/m2/hundredThreads/fiftyThreadsInOne.java
@@ -0,0 +1,43 @@
+package com.company.prog.m2.hundredThreads;
+
+import java.util.ArrayList;
+
+public class FiftyThreadsInOne extends Thread {
+    int count;
+    ArrayList<Thread> trList = new ArrayList<Thread>();
+
+
+    public FiftyThreadsInOne(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < count; i++) {
+            trList.add(new HundredThreads());
+            trList.get(i).start();
+            try {
+                trList.get(i).join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            trList.get(i).interrupt();
+
+            //System.out.println("My ID is:" + trList.get(i).getId());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FiftyThreadsInOne ftio = new FiftyThreadsInOne(50);
+        ftio.start();
+        try {
+            ftio.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ftio.interrupt();
+    }
+}
