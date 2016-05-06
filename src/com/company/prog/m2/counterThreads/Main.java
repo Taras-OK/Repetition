0d57861583162791d8c4357b06cfa3b9a199a43e
Taras-OK <taraspm35@gmail.com>
2016-05-06 21:49:03 +0300

    Counter 6.05.2016

--- src/com/company/prog/m2/counterThreads/Main.java
+++ src/com/company/prog/m2/counterThreads/Main.java
@@ -5,18 +5,57 @@ package com.company.prog.m2.counterThreads;
  */
 public class Main {
     public static class Counter extends Thread {
+        int start;
+        int end;
+
+        public Counter(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
 
         public void run() {
-            int x = 1;
-            while (x < 10)
-                System.out.println(x++);
+            int x = start;
+            int count = 0;
+            int[] a = new int[end - start + 1];
+            int[] b = new int[a.length];
+            boolean rev = false;
+            for (int i = 0; i < a.length; i++) {
+                a[i] = x++;
+                b[b.length - 1 - i] = x - 1;
+            }
+            while (count++ < 5) {
+                if (rev == false) {
+                    for (int k : a) {
+//                        try {
+//                            Thread.sleep(200);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+                        System.out.print(k + " ");
+                    }
+                    rev = true;
+                    System.out.println();
+                } else {
+                    for (int k : b) {
+//                        try {
+//                            Thread.sleep(200);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+                        System.out.print(k + " ");
+                    }
+                    rev = false;
+                    System.out.println();
+                }
+            }
         }
     }
 
     public static void main(String[] args) {
 
         try {
-            Counter c = new Counter();
+            Counter c = new Counter(105, 139);
             c.start(); // запускаем поток
             c.join(); // ждем завершения потока
             System.out.println("Thread finished");
