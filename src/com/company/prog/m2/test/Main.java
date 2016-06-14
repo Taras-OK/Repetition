62812e8fcca9fc26fdec62ae29fbf661c6981a5f
Taras <taraspm35@gmail.com>
2016-06-14 15:24:12 +0300

    test

--- src/com/company/prog/m2/test/Main.java
+++ src/com/company/prog/m2/test/Main.java
@@ -3,6 +3,7 @@ package com.company.prog.m2.test;
 public class Main {
     public static void main(String[] args) {
         String[] list={"d:\\tmp"};
+        String s = "xdf";
         MonitorDir m = new MonitorDir(list, new DirEvent());
         m.start();
     }
